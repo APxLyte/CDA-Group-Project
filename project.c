af54c86d91d0c8c1efbb947b163b163969d9b324
IsaacLaRose <ihlarose4444@gmail.com>
2025-04-11 15:23:04 -0400

    Removed duplicate storeword from instruction decode

--- project.c
+++ project.c
@@ -202,17 +202,6 @@ int instruction_decode(unsigned op,struct_controls *controls)
     controls->ALUSrc = 0;
     controls->RegWrite = 0;
   }
-  if(op == 0x0){ // storing word
-    controls->RegDst = 0;
-    controls->Jump = 0;
-    controls->Branch = 0;
-    controls->MemRead = 0;
-    controls->MemtoReg = 0;
-    controls->ALUOp = 0;
-    controls->MemWrite = 0;
-    controls->ALUSrc = 0;
-    controls->RegWrite = 0;
-  }
 }
 
 
